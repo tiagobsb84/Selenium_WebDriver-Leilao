efb72172880c5a96ad8eb836f072ce9e938465e9
Tiago Silva <tiagobsb84@gmail.com>
2022-04-05 11:17:51 -0300

    Adicionado verificacao de acesso login invalida

--- src/test/java/br/com/tiago/leilao/LoginTest.java
+++ src/test/java/br/com/tiago/leilao/LoginTest.java
@@ -3,6 +3,7 @@ package br.com.tiago.leilao;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -20,6 +21,22 @@ public class LoginTest {
 		
 		Assert.assertFalse(browser.getCurrentUrl().equals("http://localhost:8080/login"));
 		Assert.assertEquals("fulano", browser.findElement(By.id("usuario-logado")).getText());
-		//browser.quit();
+		browser.quit();
+	}
+	
+	@Test
+	public void comandosInvalidosTelaLogin() {
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+		WebDriver browser = new ChromeDriver();
+		browser.navigate().to("http://localhost:8080/login");
+		browser.findElement(By.id("username")).sendKeys("invalidos");
+		browser.findElement(By.id("password")).sendKeys("1242413");
+		browser.findElement(By.id("login-form")).submit();
+		
+		Assert.assertTrue(browser.getCurrentUrl().equals("http://localhost:8080/login?error"));
+		Assert.assertTrue(browser.getPageSource().contains("Usuário e senha inválidos."));
+		Assert.assertThrows(NoSuchElementException.class, () -> browser.findElement(By.id("usuario-logado")));
+		browser.quit();
+		
 	}
 }
